fb56ab868d9d35bd34aa4e1621d34c784a7c099d
SURAJ_KUMAR <surajk38656@gmail.com>
2024-03-14 18:28:17 +0530

    Modified ~ SURAJ KUMAR

---
+++ ArrowFunction/arrowFun.js
@@ -0,0 +1,55 @@
+// first type
+/*
+
+() => {
+  return 
+}
+
+// second type
+
+() => ()
+
+// third type
+() =>
+
+*/
+
+// first type
+let c = () => {
+    return 50;
+}
+
+console.log(c);
+console.log(c())
+
+let d = (a) => {
+    return a+a;
+}
+console.log(d(35));
+
+// second type
+
+let e = (b) => (b)
+console.log(e(12));
+
+let f = (b,a) => (a+b)
+console.log(f(12,12));
+
+// third type
+
+let g = (h) => h
+console.log(g(56));
+
+// fourth type
+
+let y = u => u
+console.log(43);
+
+
+
+
+
+
+
+
+
---
+++ SetIntervalFun/interval.js
@@ -0,0 +1,10 @@
+// SetInterval
+let a = setInterval(()=>{(document.write("Hello World!!"+"<br>"))},2000)
+setInterval(()=>{(console.log("Hello!"))},1000)
+
+// clearInterval using for stop time
+
+if(a>5)
+{
+    clearInterval(a());
+}
