3ef525e0c560aef004eb8e85a40b8f256d2f1d73
SURAJ_KUMAR <surajk38656@gmail.com>
2024-03-16 13:29:36 +0530

    Modified By SURAJ KUMAR

--- Functions/Pre-Define/TimeDate/start_stop.js
+++ Functions/Pre-Define/TimeDate/start_stop.js
@@ -1,10 +1,19 @@
+let time = document.getElementById('time');
+let date = document.getElementById('date');
 
-
-function start()
-{
-    setInterval(()=>{
+function start() {
+    x = setInterval(() => {
         let a = new Date();
         let b = a.toLocaleTimeString();
+        let c = a.toDateString();
+        time.innerHTML = b;
+        date.innerHTML = c;
         console.log(b);
-    },1000);
+    }, 1000);
+}
+
+function Stop() {
+    setTimeout(() => {
+        clearInterval(x);
+    });
 }
