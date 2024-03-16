e53dcbb2aa92f2fd9c95bcdf455f37583227a972
SURAJ_KUMAR <surajk38656@gmail.com>
2024-03-16 21:31:10 +0530

    Modified By SURAJ KUMAR

--- Arrays/arr1.js
+++ Arrays/arr1.js
@@ -1,1 +1,22 @@
-// Array :- 
+// Array :- Array is a variable that store or hold value more than 1.
+
+let arr1 = [1,2,3,5,4,7,8];
+console.log(arr1);
+
+let arr2 = ["a","b","c","d","e"];
+console.log(arr2);
+console.log(arr2[4]);
+
+console.log("Print Numbers");
+// print arr1 using for loop
+for(let i=0; i<arr1.length; i++)
+{
+    console.log(arr1[i]);
+}
+
+console.log("Print arr2");
+// print arr2 using for loop
+for(let i=0; i<arr2.length; i++)
+{
+    console.log(arr2[i]);
+}
