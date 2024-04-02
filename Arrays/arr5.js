ed92512a3ba3f77df17dd9b78231a1160e4a411f
SURAJ_KUMAR <surajk38656@gmail.com>
2024-04-02 18:22:55 +0530

    Modified By SURAJ KUMAR

---
+++ Arrays/arr5.js
@@ -0,0 +1,22 @@
+let ar = ["c++","python","php","react"];
+// ar.push(1,4,5,7);  
+// push() -> it is use for add data at the last in an arry.
+// console.log(ar); 
+
+// ar.pop();   
+// pop() -> it is use for delete last data of an array.
+// console.log(ar);
+
+// ar.unshift(2,3,6);  
+// unshift() -> it is use for add data starting indexes in an array.
+// console.log(ar);
+
+// ar.shift();
+// shift() -> it is use for delete data of first index of an array.
+// console.log(ar);
+
+console.log("length : ",ar.length);
+
+ar1 = [1,4,6,8,5];
+let concat = ar.concat(ar1);
+console.log(concat);
