b13d9d2eeaa2da645c38e5ac0c84027d7568e6cc
SURAJ_KUMAR <surajk38656@gmail.com>
2024-04-10 18:06:08 +0530

    Modified By SURAJ KUMAR

---
+++ Map/filter.js
@@ -0,0 +1,24 @@
+/*
+filter functions
+*/
+
+let employee = [
+    {
+        namee:"Suraj",
+        age:23,
+        status:"single"
+    },
+    {
+        namee:"Rajan",
+        age:24,
+        status:"mingle"
+    },
+    {
+        namee:"Amit",
+        age:25,
+        status:"married"
+    },
+    ];
+
+    let fltr = employee.filter((t)=>{ return t.age>23 && t.age<25});
+    console.log(fltr);
--- Map/map.js
+++ Map/map.js
@@ -1,4 +1,31 @@
 /*
 Map():-
-
+it is used to fetching data
 */
+
+let employee = [
+{
+    namee:"Suraj",
+    age:24,
+    status:"single"
+},
+{
+    namee:"Rajan",
+    age:24,
+    status:"mingle"
+},
+{
+    namee:"Amit",
+    age:24,
+    status:"married"
+},
+];
+
+let p = employee.map((i)=> { return i});  
+let q = employee.map((i)=> { return i.namee});  // print name
+let r = employee.map((i)=> { return Object.values(i)});  // print values
+console.log(p);
+console.log(q);
+console.log(r);
+
+
