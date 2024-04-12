a26a042a304ec20d0fa80345f8b1d91d4192bb58
SURAJ_KUMAR <surajk38656@gmail.com>
2024-04-12 13:53:18 +0530

    Modified By SURAJ KUMAR

--- Test-1/validation.js
+++ Test-1/validation.js
@@ -1,41 +1,50 @@
 
-function valid()
-{
-    let names = document.getElementById('name').value;
+function valid(){
+    let n = document.getElementById('name').value;
     let contact = document.getElementById('contact').value;
     let pass = document.getElementById('pass').value;
-    let conf_pass = document.getElementById('pass').value;
+    let cpass = document.getElementById('conf_pass').value;
 
-    if(names == "")
+    if(n=="")
     {
-        prompt("please enter name: ");
+        alert("please enter your name");
+        document.getElementById('name').focus();
         return false;
     }
-    else if(contact.lenhth<10 || contact.lenhth>10)
+    else if(contact.length<10 || contact.length>10)
     {
-        alert("please enter contact number in 10 digit");
+        alert("Please enter 10 digit");
         document.getElementById('contact').focus();
         return false;
     }
-    else if(isNaN(contact)){
-        alert = "please enter value";
-        document.getElementById('contact').focus();
+    else if(isNaN(contact))
+    {
+        alert("please enter your contact number");
         return false;
     }
     else if(pass=="")
     {
-        alert("enter the number ");
-        pass.document.getElementById('pass').focus();
+        alert("Please enter your password");
+        document.getElementById('pass').focus();
         return false;
     }
-    else if(conf_pass=="")
+
+    else if(!(pass.match(/[@#!$%]/)))
     {
-        alert("enter the number ");
-        pass.document.getElementById('conf_pass').focus();
+        alert("your password is weak");
+        document.getElementById('pass').focus();
         return false;
     }
-    else if(pass!=conf_pass)
+    else if(cpass=="")
     {
-        alert("Password not matching")
+        alert("Please enter your confPass password");
+        document.getElementById('conf_pass').focus();
+        return false;
+    }
+    else if(pass !=cpass)
+    {
+        alert("password not match!!");
+        document.getElementById('conf_pass').focus();
+        return false;
     }
 }
