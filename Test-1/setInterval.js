3bb67b807163abaf545224865a71be7301fbf3af
SURAJ_KUMAR <surajk38656@gmail.com>
2024-04-12 14:02:28 +0530

    Modified By SURAJ KUMAR

--- Test-1/setInterval.js
+++ Test-1/setInterval.js
@@ -3,8 +3,9 @@ let a = setInterval(()=>{
    let time = new Date();
 //    console.log(time.toTimeString());
    console.log(time.toLocaleTimeString());
+   console.log(time.toUTCString())
 },1000)
 
-setTimeout(() => {
+    setTimeout(() => {
     clearInterval(a);
-}, 5000);
+}, 10000);
