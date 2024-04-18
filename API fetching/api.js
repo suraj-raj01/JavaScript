c02ffec5fc19c9dfafe0416d4c7e64ae7a89cbc3
SURAJ_KUMAR <surajk38656@gmail.com>
2024-04-18 18:34:06 +0530

    API Fetching

---
+++ API fetching/api.js
@@ -0,0 +1,9 @@
+async function fet()
+{
+    let n = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let result = await n.json();
+    console.log(result,"\n")
+    console.log(result.map((e) => e.title));
+    console.log(result.map((e) => e.id));
+}
+fet();
