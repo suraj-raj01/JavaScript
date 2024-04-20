833894c5e828b10c25fa756306d75438cf61ec0a
SURAJ_KUMAR <surajk38656@gmail.com>
2024-04-21 00:46:56 +0530

    Modified By SURAJ KUMAR

---
+++ ArrObject/arrObj2.js
@@ -0,0 +1,45 @@
+let Student = [
+    {
+        id:101,
+        Student_name:"SURAJ KUMAR",
+        age:20,
+        Location:"Bhopal"
+    },
+    {
+        id:102,
+        Student_name:"RAJAN KUMAR",
+        age:21,
+        Location:"Bihar"
+    },
+    {
+        id:103,
+        Student_name:"BITTU KUMAR",
+        age:23,
+        Location:"Bhopal"
+    },
+    {
+        id:104,
+        Student_name:"RAHUL KUMAR",
+        age:20,
+        Location:"Purnea"
+    },
+    {
+        id:105,
+        Student_name:"HARIOM KUMAR",
+        age:22,
+        Location:"Bihar"
+    }
+]
+console.table(Student);
+
+/*
+┌─────────┬─────┬────────────────┬─────┬──────────┐
+│ (index) │ id  │ Student_name   │ age │ Location │
+├─────────┼─────┼────────────────┼─────┼──────────┤
+│ 0       │ 101 │ 'SURAJ KUMAR'  │ 20  │ 'Bhopal' │
+│ 1       │ 102 │ 'RAJAN KUMAR'  │ 21  │ 'Bihar'  │
+│ 2       │ 103 │ 'BITTU KUMAR'  │ 23  │ 'Bhopal' │
+│ 3       │ 104 │ 'RAHUL KUMAR'  │ 20  │ 'Purnea' │
+│ 4       │ 105 │ 'HARIOM KUMAR' │ 22  │ 'Bihar'  │
+└─────────┴─────┴────────────────┴─────┴──────────┘
+*/
