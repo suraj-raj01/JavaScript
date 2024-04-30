a580d5cd22890016587878fe43cfaf04070c5205
SURAJ_KUMAR <surajk38656@gmail.com>
2024-04-30 17:28:17 +0530

    Modified By SURAJKUAMR

---
+++ Arrays/arr6.js
@@ -0,0 +1,13 @@
+// slice return data in array format
+// slice() -> 
+// let arr = [1,2,3,4,5,6,7];
+// let ans = arr.slice(1,3);
+// console.log(ans);
+
+
+let ar2 = [1,2,3,4,5];
+// let b = ar2.splice(1,3);
+// console.log(b);
+let c = ar2.splice(1,3,"suraj");
+console.log(c);
+console.log(ar2);
---
+++ FUNCTION/script.js
@@ -0,0 +1,6 @@
+function fun()
+{
+    let a = parseInt(prompt("enter first number: "));
+    let b = parseInt(prompt("enter second number: "));
+    console.log(a+b);
+}
