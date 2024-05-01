c9d776d78011a30c8c369000b42a85bc2172a2ef
SURAJ_KUMAR <surajk38656@gmail.com>
2024-05-01 17:19:46 +0530

    Modified By SURAJ KUMAR

---
+++ FUNCTION/calculation.js
@@ -0,0 +1,47 @@
+function calculate()
+{
+    let a = parseInt(prompt("enter the number: \n1 -> add\n2 -> sub\n3 ->mul\n4 ->div"));
+    switch(a)
+    {
+        case 1:
+            add();
+            break;
+        case 2:
+            sub();
+            break;
+        case 3:
+            mul();
+            break;
+        case 4:
+            div();
+            break;
+        default:
+            console.log("please enter valid!!!");
+    }
+
+    function add()
+    {
+        let x = parseInt(prompt("Enter the first number: "));
+        let y = parseInt(prompt("Enter the second number: "));
+        console.log(x+y);
+    }
+    function sub()
+    {
+        let x = parseInt(prompt("Enter the first number: "));
+        let y = parseInt(prompt("Enter the second number: "));
+        console.log(x-y);
+    }
+    function mul()
+    {
+        let x = parseInt(prompt("Enter the first number: "));
+        let y = parseInt(prompt("Enter the second number: "));
+        console.log(x*y);
+    }
+    function div()
+    {
+        let x = parseInt(prompt("Enter the first number: "));
+        let y = parseInt(prompt("Enter the second number: "));
+        console.log(x/y);
+    }
+}
+calculate();
--- FUNCTION/script.js
+++ FUNCTION/script.js
@@ -1,6 +1,27 @@
-function fun()
+// function fun()   // declaration
+// {
+//     let a = parseInt(prompt("enter first number: "));
+//     let b = parseInt(prompt("enter second number: "));
+//     console.log(a+b);
+//     //definition
+// }
+// function calling 
+
+// function f1(a,b)
+// {
+//     // console.log(a+b);
+//     return a+b;
+// }
+// console.log(f1(10,20));
+// let c = f1(10,20);
+// console.log(f1(c))
+
+
+// WAP to find Area of circle using function with argument with value take input by user
+let a = parseInt(prompt("enter the area: "));
+function area(a)
 {
-    let a = parseInt(prompt("enter first number: "));
-    let b = parseInt(prompt("enter second number: "));
-    console.log(a+b);
+    return 3.14*a*a;
 }
+let x = area(a);
+console.log(x);
