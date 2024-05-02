ce2de2ed7d256fde041abdb4af1637a1d268baf0
SURAJ_KUMAR <surajk38656@gmail.com>
2024-05-02 17:27:45 +0530

    Modified By Suraj Kumar

---
+++ DOM/dom.js
@@ -0,0 +1,26 @@
+let r = document.getElementById('heading');
+r.innerHTML = "CYBROM";
+r.style.backgroundColor='green';
+r.style.padding='10px';
+r.style.color='white'
+
+let para = document.getElementById('para');
+para.innerHTML = "HATE💔";
+let h2 = document.getElementById('h2');
+h2.innerHTML = "LOVE❤️";
+h2.style.backgroundColor='green';
+h2.style.padding='10px';
+h2.style.color='white'
+
+para.style.backgroundColor='black';
+para.style.padding='15px';
+para.style.color='white';
+para.style.fontSize='22px';
+para.style.fontWeight='bold'
+
+
+let img = document.getElementById('img');
+img.src = "logo1.png";
+img.style.backgroundColor='gold';
+img.style.margin='auto'
+img.style.padding='10px';
--- FUNCTION/calculation.js
+++ FUNCTION/calculation.js
@@ -1,4 +1,4 @@
-let ans = document.getElementById('res');
+
 function calculate() {
     let a = parseInt(prompt("enter the number: \n1 -> add\n2 -> sub\n3 ->mul\n4 ->div"));
     switch (a) {
@@ -23,7 +23,7 @@ function calculate() {
         let x = parseInt(prompt("Enter the first number: "));
         let y = parseInt(prompt("Enter the second number: "));
         console.log(x + y);
-        document.write(x+y);
+        document.write(x + y);
     }
     function sub() {
         let x = parseInt(prompt("Enter the first number: "));
@@ -41,4 +41,3 @@ function calculate() {
         console.log(x / y);
     }
 }
-calculate();
