5c7a7f8b2c5ceafc9d26422624205f0abdf44aca
SURAJ_KUMAR <surajk38656@gmail.com>
2024-05-04 01:03:53 +0530

    Modified By SURAJ KUMAR

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,31 +1,31 @@
 let r = document.getElementById('heading');
 r.innerHTML = "CYBROM";
-r.style.backgroundColor='green';
-r.style.padding='10px';
-r.style.color='white'
+r.style.backgroundColor = 'green';
+r.style.padding = '10px';
+r.style.color = 'white'
 
 let para = document.getElementById('para');
 para.innerHTML = "HATE💔";
 
 let h2 = document.getElementById('h2');
 h2.innerHTML = "LOVE❤️";
-h2.style.backgroundColor='green';
-h2.style.padding='10px';
-h2.style.color='white'
+h2.style.backgroundColor = 'green';
+h2.style.padding = '10px';
+h2.style.color = 'white'
 
 //paragraph
-para.style.backgroundColor='black';
-para.style.padding='15px';
-para.style.color='white';
-para.style.fontSize='22px';
-para.style.fontWeight='bold'
+para.style.backgroundColor = 'black';
+para.style.padding = '15px';
+para.style.color = 'white';
+para.style.fontSize = '22px';
+para.style.fontWeight = 'bold'
 
 //image
 let img = document.getElementById('img');
 img.src = "logo1.png";
-img.style.backgroundColor='gold';
-img.style.margin='auto'
-img.style.padding='10px';
+img.style.backgroundColor = 'gold';
+img.style.margin = 'auto'
+img.style.padding = '10px';
 
 
 // getElementByClassName
@@ -38,13 +38,13 @@ let tag = document.getElementsByTagName('span');
 console.log(tag[0]);
 
 let body = document.getElementsByTagName('body');
-document.body.style.backgroundColor='lime';
+document.body.style.backgroundColor = 'orangered';
 
-let li = document.getElementsByTagName('li');
-for(let i=0; i<8; i++)
-    {
-        if(li[i]%2!=0)
-            {
-                li[i].style.backgroundColor='red';
-            }
+let x = document.getElementsByTagName('li');
+for (let i = 0; i < 8; i++) {
+    if (x[i] % 2 != 0) {
+        x[i].style.backgroundColor = 'black';
+        x[i].style.fontWeight = 'bold';
+        x[i].style.color = 'white';
     }
+}
---
+++ DOM/test.js
@@ -0,0 +1,10 @@
+let m = document.getElementsByTagName('div')
+for(let i=0; i<5; i++)
+    {
+        if(m[i]%2!=0)
+            {
+                m[i].style.backgroundColor='lime'
+                m[i].style.fontWeight='bold'
+                m[i].style.padding='0px 5px'
+            }
+    }
