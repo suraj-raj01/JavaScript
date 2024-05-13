3f11ac7d7c03d9b2143a664d92fd033261d6f078
SURAJ_KUMAR <surajk38656@gmail.com>
2024-05-13 17:30:02 +0530

    Form Validation

---
+++ Form/form2.js
@@ -0,0 +1,60 @@
+function valid() {
+    let n = document.getElementById('name').value;
+    let age = document.getElementById('age').value;
+    let email = document.getElementById('email').value;
+    let contact = document.getElementById('contact').value;
+    let pass = document.getElementById('pass').value;
+    let conf_pass = document.getElementById('conf-pass').value;
+
+    if (n == "") {
+        alert("please enter your name");
+        document.getElementById('name').focus();
+        return false;
+    }
+    else if (age == "") {
+        alert("please enter your age");
+        document.getElementById('age').focus();
+        return false;
+    }
+    else if (isNaN(age)) {
+        alert("only number accepted")
+        document.getElementById('age');
+        return false;
+    }
+    else if (email == "") {
+        alert("please enter email");
+        document.getElementById('email').focus();
+        return false;
+    }
+    else if (contact == "") {
+        alert("please enter contact number");
+        document.getElementById('contact').focus();
+        return false;
+    }
+
+    else if (isNaN(contact)) {
+        alert("only number accepted")
+        document.getElementById('contact');
+        return false;
+    }
+    else if (contact.length<10 || contact.length>10) {
+        alert("please enter 10 digit number")
+        document.getElementById('contact').focus();
+        return false;
+    }
+    else if (pass == "") {
+        alert("please enter password");
+        document.getElementById('pass').focus();
+        return false;
+    }
+    else if (conf_pass == "") {
+        alert("Please enter conform password");
+        document.getElementById("conf-pass").focus();
+        return false;
+    }
+    else if (pass != conf_pass) {
+        alert("Incorrect Password!!");
+        document.getElementById('conf-pass').focus();
+        return false;
+    }
+}
