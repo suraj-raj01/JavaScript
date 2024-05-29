1728c5dbb510564f1edc22b13431cf6ebcbebcab
SURAJ_KUMAR <surajk38656@gmail.com>
2024-05-29 17:11:33 +0530

    String Function

---
+++ FUNCTION/str.js
@@ -0,0 +1,29 @@
+let a = "suraj";
+console.log(a);
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+
+let b = a.repeat(3);
+console.log(b);
+
+let c = a.substring(2,5);
+console.log(c);
+
+let d = a.charAt(0);
+console.log(d);
+
+let e = "hello... \nthis is javascript";
+console.log(e);
+
+let f = `Hello 
+    this is 
+        Js Class`;
+console.log(f);
+// alert(f);
+
+let nm = "Quasim Sir";
+let g = `hello this is 
+    js class by ${nm}!!! 
+not bad`;
+console.log(g);
+
