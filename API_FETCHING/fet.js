1fb8f97004c6c8f8a67656e24fcd556881ebce65
SURAJ_KUMAR <surajk38656@gmail.com>
2024-06-03 17:26:50 +0530

    Api Fetching

--- API_FETCHING/fet.js
+++ API_FETCHING/fet.js
@@ -1,9 +1,16 @@
 async function fet()
 {
+    let sh = document.getElementById("showData")
     let a = await fetch("https://jsonplaceholder.typicode.com/posts");
     let b = await a.json();
-    let p = b.map((e)=>{
-        return e.id
-    });
-    console.log(p);
+    let ot = "";
+    let p = b.map((e)=>
+    `
+    <tr>
+    <td> ${e.id} </td>
+    <td> ${e.userId} </td>
+    <td> ${e.title} </td>
+    </tr>
+    `).join(" ");
+    sh.innerHTML = p;
 }
