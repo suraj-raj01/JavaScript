dc02d5770414571bb6d5b4c72f5fa4fab3e10118
SURAJ_KUMAR <surajk38656@gmail.com>
2024-06-04 17:32:16 +0530

    API_FETCHING

--- API_FETCHING/fet.js
+++ API_FETCHING/fet.js
@@ -1,14 +1,14 @@
 async function fet()
 {
     let sh = document.getElementById("showData")
-    let a = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let a = await fetch("http://localhost:3000/student");
     let b = await a.json();
     let p = b.map((e)=>
     `
     <tr>
     <td> ${e.id} </td>
-    <td> ${e.userId} </td>
-    <td> ${e.title} </td>
+    <td> ${e.pr_name} </td>
+    <td> ${e.price} </td>
     </tr>
     `).join(" ");
     sh.innerHTML = p;
