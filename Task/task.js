4e5ea92060bc466c0fe3041105c0278657c9b1c0
SURAJ_KUMAR <surajk38656@gmail.com>
2024-06-13 20:23:24 +0530

    Food Order

--- Task/task.js
+++ Task/task.js
@@ -2,3 +2,54 @@ let b = document.getElementsByTagName('body');
 function mode() {
     document.body.classList.toggle('demo')
 }
+
+let i = 0
+let a = 0;
+function fun1()
+{
+    x = 230;
+    a+=x;
+    i++;
+    alert("Item Added");
+    document.getElementById("res").innerHTML=a;
+    document.getElementById("itm").innerHTML=i;
+}
+
+function fun2()
+{
+    x = 330;
+    a+=x;
+    i++;
+    alert("Item Added");
+    document.getElementById("res").innerHTML=a;
+    document.getElementById("itm").innerHTML=i;
+}
+function fun3()
+{
+    x = 299;
+    a+=x;
+    i++;
+    alert("Item Added");
+    document.getElementById("res").innerHTML=a;
+    document.getElementById("itm").innerHTML=i;
+}
+function fun4()
+{
+    x = 399;
+    a+=x;
+    i++;
+    alert("Item Added");
+    document.getElementById("res").innerHTML=a;
+    document.getElementById("itm").innerHTML=i;
+}
+
+
+function display()
+{
+    document.getElementById("item").style.display = "block";
+}
+
+function hide()
+{
+    document.getElementById("item").style.display = "none";
+}
