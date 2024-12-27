a82cad21fb05c64f7bc74c9e31bfcf8f1d8ea2ab
SURAJ_KUMAR <surajk38656@gmail.com>
2024-12-27 14:11:01 +0530

    Practice javascript

--- Practice/tricky.js
+++ Practice/tricky.js
@@ -1,4 +1,5 @@
 // fibonacci series
+/*
 let a=0;
 let b=1;
 console.log(a);
@@ -13,4 +14,30 @@ function fib(n,a,b){
     console.log(c);
     fib(n-1,b,c);
 }
+*/
+
+// Binary Search
+/*
+function binarySearch(arr,target){
+    let start=0;
+    let end = arr.length-1;
+    while(start<=end){
+        let mid = Math.floor((start+end)/2);
+        if(arr[mid]==target){
+            return mid;
+        }
+        else if(arr[mid]>target){
+            end = mid-1;
+        }
+        else{
+            start = mid+1;
+        }
+    }
+    return -1;
+}
+let arr = [1,5,8,9,45,78];
+const target = 45;
+const result = binarySearch(arr,target);
+console.log(result);
+*/
 
